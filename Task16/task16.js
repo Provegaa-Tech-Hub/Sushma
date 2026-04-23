18aa434f37963778cb66a41bb45dafaa7289afa4
mangamuthinti <mangayyamma.07@gmail.com>
2026-04-23 22:57:35 +0530

    commit

--- Task16/task16.js
+++ Task16/task16.js
@@ -43,7 +43,7 @@ function checkpassword() {
 
 const originalText = document.getElementById("text").innerText;
 function highlight() {
-    let keyword = document.getElementById("highlightsearch").value;//user input
+    let keyword = document.getElementById("search").value;//user input
     let textElement = document.getElementById("text");
 
     // reads paragraph text
@@ -55,8 +55,52 @@ function highlight() {
     let regex = new RegExp(`(${keyword})`, "gi");//g->global(allmatches) i->case -insensitive
 
     textElement.innerHTML = originalText.replace(regex, '<span class="highlight">$1</span>');//wraps matched word in <span>
-    //textElement.innerText = newText;// displays highlighted text
+    textElement.innerText = newText;// displays highlighted text
 
 }
 
+//clock
 
+function showTime() {
+    let now = new Date();
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+    hours = hours < 10 ? "0" + hours : hours;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    let time = hours + ":" + minutes + ":" + seconds;
+    document.getElementById("clock").innerText = time;
+}
+setInterval(showTime, 1000);
+
+//Timer
+
+let count = 0;                //starts from 0
+let intervel;                 //store timer
+
+function start() {  //start button clicked
+    if(!intervel){      //is timer already running no timer running
+          intervel = setInterval(() => {      //starts repeating loop
+        count++;                               //increases value by 1
+        document.getElementById("timer").innerText = count;
+    }, 1000); //runs every 1000ms=1s
+
+    } 
+}
+
+function stop() {
+    clearInterval(intervel);
+    intervel = null;          //resets interval variable
+}
+
+//Date Formatting
+function showdate() {                     //function name
+    let now = new Date();                     //creates date obj and gets date from the system
+    let day = now.getDate();                //extracts date of the month
+    let month = now.getMonth() + 1;            //returns month 0-11 so we add +1
+    let year = now.getFullYear();               //gets full year
+    let result = day + "/" + month + "/" + year;//combines values into string
+    document.getElementById("result").innerText = result;//dispaly the date
+}
