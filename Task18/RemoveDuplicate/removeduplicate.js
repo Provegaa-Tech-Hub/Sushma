a88be5d5bc2bb06b97168084f48d25743cd56c67
mangamuthinti <mangayyamma.07@gmail.com>
2026-04-29 10:01:39 +0530

    Update removeduplicate.js

--- Task18/RemoveDuplicate/removeduplicate.js
+++ Task18/RemoveDuplicate/removeduplicate.js
@@ -2,8 +2,8 @@ function removeDuplicate() {
     let input = document.getElementById("inputArray").value;
     let arr = input.split(",").map(item => item.trim());
 
-    let uniqueArr= [...new Set(arr)];
-    document.getElementById("result").innerText="Result:"+uniqueArr.join(",");
+    let uniqueArr = [...new Set(arr)];
+    document.getElementById("result").innerText = "Result:" + uniqueArr.join(",");
 
-
+    document.getElementById("inputArray").value = "";
 }
