4e7efe1e3311f57a86e608c130f5e92aba527423
mangamuthinti <mangayyamma.07@gmail.com>
2026-05-19 09:36:51 +0530

    commit

--- Delicio/src/IconsBlock.jsx
+++ Delicio/src/IconsBlock.jsx
@@ -26,16 +26,14 @@ function IconBlocks() {
             <div className="card-main">
                 <div className="card card-styles" >
                     <img
-                        src="./Images/product1.jpg"className="card-img-top" alt="product" />
+                        src="./Images/product1.jpg" className="card-img-top" alt="product" />
                     <div className="card-body">
                         <a href="#" className="card-text" >
-                           <span>Delicious Chocolate</span> 
+                            <span>Delicious Chocolate</span>
                         </a>
                         <p>
                             $ 23.00
                         </p>
-
-                        
                     </div>
                 </div>
                 <div className="card card-styles">
@@ -48,11 +46,11 @@ function IconBlocks() {
                     <div className="card-body">
 
                         <a href="#" className="card-text">
-                           Mix of Chocolates
+                            Mix of Chocolates
                         </a>
 
                         <p >
-                          $ 32.00
+                            $ 32.00
                         </p>
                     </div>
                 </div>
@@ -66,7 +64,7 @@ function IconBlocks() {
                     <div className="card-body">
 
                         <a href="#" className="card-text">
-                           Choco Nuts
+                            Choco Nuts
                         </a>
 
                         <p>
@@ -82,17 +80,134 @@ function IconBlocks() {
                     />
 
                     <div className="card-body">
-                        <a href="#"className="card-text">
-                           Soft Cake.
+                        <a href="#" className="card-text">
+                            Soft Cake.
                         </a>
 
                         <p >
-                          $ 15.00
+                            $ 15.00
                         </p>
                     </div>
                 </div>
             </div>
 
+            <div className="choco-conatiner">
+                <div id="carouselExample" className="carousel slide" data-bs-ride="carousel">
+                    <div className="carousel-inner">
+                        <div className="carousel-item active">
+                            <div className="d-flex justify-content-center align-items-center gap-5 client-slider">
+
+                                <img src="./Images/client1.png" className="brand-img" alt="client1" />
+
+                                <img src="./Images/client2.png" className="brand-img" alt="client2" />
+
+                                <img src="./Images/client3.png" className="brand-img" alt="client3" />
+
+                                <img src="./Images/client4.png" className="brand-img" alt="client4" />
+
+                            </div>
+                        </div>
+                        <div className="carousel-item">
+                            <div className="d-flex justify-content-center align-items-center gap-5 client-slider">
+
+                                <img src="./Images/client1.png" className="brand-img" alt="client1" />
+
+                                <img src="./Images/client2.png" className="brand-img" alt="client2" />
+
+                                <img src="./Images/client3.png" className="brand-img" alt="client3" />
+
+                                <img src="./Images/client4.png" className="brand-img" alt="client4" />
+
+                            </div>
+                        </div>
+
+                    </div>
+                    <button
+                        className="carousel-control-prev"
+                        type="button"
+                        data-bs-target="#carouselExample"
+                        data-bs-slide="prev"
+                    >
+                        <i className="fa-regular fa-circle-left"></i>
+                    </button>
+                    <button
+                        className="carousel-control-next"
+                        type="button"
+                        data-bs-target="#carouselExample"
+                        data-bs-slide="next"
+                    >
+                        <i className="fa-regular fa-circle-right"></i>
+                    </button>
+                </div>
+            </div>
+
+            <div className="card-main">
+                <div className="card card-styles" >
+                    <img
+                        src="./Images/product1.jpg" className="card-img-top" alt="product" />
+                    <div className="card-body">
+                        <a href="#" className="card-text" >
+                            <span>CC Circualr Cake</span>
+                        </a>
+                        <p>
+                            $ 55.00
+                        </p>
+                    </div>
+                </div>
+                <div className="card card-styles">
+                    <img
+                        src="./Images/product2.jpg"
+                        className="card-img-top"
+                        alt="product"
+                    />
+
+                    <div className="card-body">
+
+                        <a href="#" className="card-text">
+                           Mixy Choco
+                        </a>
+
+                        <p >
+                            $ 12.00
+                        </p>
+                    </div>
+                </div>
+                <div className="card card-styles" >
+                    <img
+                        src="./Images/product3.jpg"
+                        className="card-img-top"
+                        alt="product"
+                    />
+
+                    <div className="card-body">
+
+                        <a href="#" className="card-text">
+                           All Nuts Choco
+                        </a>
+
+                        <p>
+                            $ 26.00
+                        </p>
+                    </div>
+                </div>
+                <div className="card card-styles" >
+                    <img
+                        src="./Images/product4.jpg"
+                        className="card-img-top"
+                        alt="product"
+                    />
+
+                    <div className="card-body">
+                        <a href="#" className="card-text">
+                            Tasty Chocolate
+                        </a>
+
+                        <p >
+                            $ 23.00
+                        </p>
+                    </div>
+                </div>
+            </div>
 
         </div>
     )
