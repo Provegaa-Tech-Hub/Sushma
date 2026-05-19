3defdde480cc44c5b1b989425aebe9df0ad353da
mangamuthinti <mangayyamma.07@gmail.com>
2026-05-19 19:27:59 +0530

    done

--- ChariticsPage/src/pages/ContactUs.jsx
+++ ChariticsPage/src/pages/ContactUs.jsx
@@ -12,18 +12,42 @@ function ContactUs() {
                 <div className="row">
                     <div className="col">
                         <div className="contact-info">
+                            <div className="icon-call">
+                               <i class="fa-solid fa-phone-volume call-icon"></i>
+                            </div>
+                            <div className="txt">
+                                <span className="title">Phone number</span>
+                                <span className="phone">+44 204 577 0077</span>
+
+                            </div>
 
                         </div>
 
                     </div>
                     <div className="col">
                         <div className="contact-info">
+                            <div className="icon-call">
+                               <i class="fa-solid fa-comment-dots"></i>
+                            </div>
+                            <div className="txt">
+                                <span className="title">Email address </span>
+                                <span className="phone">fandi@gmail.com</span>
+
+                            </div>
 
                         </div>
 
                     </div>
                     <div className="col">
                         <div className="contact-info">
+                            <div className="icon-call">
+                              <i class="fa-solid fa-location-dot location-icon"></i>
+                            </div>
+                            <div className="txt">
+                                <span className="title">Office Address</span>
+                                <span className="phone">Washington Ave, NY</span>
+
+                            </div>
 
                         </div>
 
